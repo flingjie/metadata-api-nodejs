3414186254b4272b3a7408a3c813658674e9cec0
lingjie fan <fanlingjie@laiye.com>
2019-04-01 23:26:18 +0800

    update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res) {
-  res.send('Get ready for OpenSea!');
+  res.send('Get ready for CryptoPiggies !');
 })
 
 app.get('/api/token/:token_id', function(req, res) {
@@ -23,6 +23,8 @@ app.get('/api/token/:token_id', function(req, res) {
   const pig = db[tokenId]
   const data = {
     'name': pig.name,
+    'description': "加密猪",
+    'background_color': "ffffff",
     'attributes': pig.attrs,
     'image': `${HOST}/images/${tokenId}.png`
   }
@@ -33,16 +35,3 @@ app.get('/api/token/:token_id', function(req, res) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 })
-
-// returns the zodiac sign according to day and month ( https://coursesweb.net/javascript/zodiac-signs_cs )
-function zodiac(day, month) {
-  var zodiac =['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
-  var last_day =['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
-  return (day > last_day[month]) ? zodiac[month*1 + 1] : zodiac[month];
-}
-
-function monthName(month) {
-  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-  ]
-  return monthNames[month - 1]
-}
